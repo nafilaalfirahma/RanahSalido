fdfbd8cc748726f1b57f79176a559989b7b2db95
fathimabida <fathimahabidah@student.telkomuniversity.ac.id>
2023-12-24 18:24:27 +0700

    update

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,202 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Ranah Salido</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+<!-- Carousel di Bagian Atas -->
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="9500">
+                <img src="carousel1.png" class="d-block w-100" alt="...">
+
+                <!-- Caption di dalemnya -->
+                <div class="carousel-caption d-none d-md-block">
+                   <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p> 
+                </div>
+
+            </div>
+            
+            <div class="carousel-item" data-bs-interval="9500">
+            <img src="carousel2.png" class="d-block w-100" alt="...">
+
+            <!-- Caption di dalemnya -->
+            <div class="carousel-caption d-none d-md-block">
+            <h5>Welcome</h5>
+                 <p>Get to know more about us!</p>
+            </div>
+            </div>
+
+            <div class="carousel-item" data-bs-interval="9500">
+            <img src="carousel3.png" class="d-block w-100" alt="...">
+
+            <!-- Caption di dalemnya -->
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Welcome</h5>
+                 <p>Get to know more about us!</p>
+            </div>
+            </div>
+
+            <div class="carousel-item" data-bs-interval="9500">
+            <img src="carousel4.png" class="d-block w-100" alt="...">
+
+            <!-- Caption di dalemnya -->
+            <div class="carousel-caption d-none d-md-block">
+            <h5>Welcome</h5>
+                 <p>Get to know more about us!</p>
+            </div>
+            </div>
+
+            <div class="carousel-item" data-bs-interval="9500">
+            <img src="carousel5.png" class="d-block w-100" alt="...">
+
+            <!-- Caption di dalemnya -->
+            <div class="carousel-caption d-none d-md-block">
+            <h5>Welcome</h5>
+                 <p>Get to know more about us!</p>
+            </div>
+            </div>
+
+        </div>
+  </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
+    <!-- Buat About Us -->
+    <br>
+    <div class="card text-center">
+    <div class="card-body">
+        <h3>Tentang Kami</h3>
+    </div>
+    </div>
+
+<!-- Content Dashboardnya -->
+<!-- Pegawai Pekerja, Luas Tanah, Jenis Bibit-->
+<div class="container mt-4">
+    <div class="row">
+      <div class="col-md-4">
+        <!-- Kolom 1 -->
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title text-center">Jumlah Pekerja & Petani</h4>
+            <center>
+            <img src="pekerja.png" alt="Logo" width="80" height="80" class="d-inline-block">
+            <br>
+            <h1>350</h1>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <!-- Kolom 2 -->
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title text-center">Jenis Bibit</h4>
+            <center>
+            <img src="bibit.png" alt="Logo" width="80" height="80" class="d-inline-block">
+            <br>
+            <h1>3</h1>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <!-- Kolom 3 -->
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title text-center">Luas Lahan</h4>
+            <center>
+            <img src="lahan.png" alt="Logo" width="80" height="80" class="d-inline-block">
+            <br>
+            <h1>56.850 ha</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Profil Perusahaan -->
+  <br>
+    <div class="card text-center">
+    <div class="card-body">
+        <h3>Visi & Misi</h3>
+    </div>
+    </div>
+
+<!-- Visi Misi -->
+<div class="container mt-4">
+    <div class="row">
+      <div class="col-md-6">
+        <!-- Kolom 1 -->
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title text-center">Visi</h4>
+            <center>
+            <br>
+            <h3>"Menjadi kelompok tani pengelola perkebunan kelapa sawit yang unggul dan berkelanjutan, memberikan kontribusi positif bagi kesejahteraan anggota, meningkatkan produktivitas perkebunan, dan berperan aktif dalam pelestarian lingkungan serta pembangunan komunitas."</h3>
+          </div>
+        </div>
+      </div>
+      <br>
+      <br>
+
+      <div class="col-md-6">
+        <!-- Kolom 2 -->
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title text-center">Misi</h4>
+            <br>
+            <h6>1. Melakukan pengelolaan perkebunan kelapa sawit secara berkelanjutan dengan menerapkan praktik pertanian yang ramah lingkungan, memperhatikan keseimbangan ekosistem dan menjaga keberlanjutan produksi.</h6>
+            <br>
+            <h6>2. Mendorong partisipasi aktif anggota dalam pengambilan keputusan kelompok dan pengembangan inisiatif bersama.</h6>
+            <br>
+            <h6>3. Membangun kemitraan yang kuat dengan pihak terkait, termasuk pemerintah, lembaga riset pertanian, dan pelaku industri, guna mendukung pertumbuhan dan pengembangan kelompok tani.</h6>
+          </div>
+        </div>
+      </div>
+      <br>
+
+      <br>
+      <!-- Sejarah Perusahaan -->
+      <br>
+    <div class="card text-center">
+    <div class="card-body">
+        <h3>Sejarah Perusahaan</h3>
+        <br>
+                    <p class="card-text">Deskripsi sejarah perusahaan dan perjalanan kelompok tani dapat ditambahkan di sini.</p>
+                    <br>
+    </div>
+    </div>
+
+<br>
+<!-- Kontak Perusahaan -->
+<br>
+<div class="card text-center bg-success text-white">
+    <div class="card-body">
+        <h3>Hubungi Kami</h3>
+        <br>
+                    <p class="card-text">Alamat: Jl. Contoh No. 123, Kota Contoh</p>
+                    <p class="card-text">Telepon: (012) 345-6789</p>
+                    <p class="card-text">Email: info@rantanisalido.com</p>
+    </div>
+</div>
+
+      
+
+  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="app.css" />
+    <title>Dashboard</title>
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <div class="logo"><a href="#">Ranah Salido</a></div> 
+            <ul class="links">
+                <li><a href="dashboard">Home</a></li>
+                <li><a href="tentangKami">About Us</a></li>
+                <li><a href="pengelolaankebun">Pengelolaan Perkebunan</a></li>
+                <li><a href="formPengajuanInvestasi">Pengajuan Investasi</a></li>
+                <li><a href="formPengajuanPembelian">Pengajuan Pembelian</a></li>
+            </ul>
+            <a href="#" class="action_btn">Sign Up</a>
+            <div class="toggle">
+                <i class="fa-light fa-bars"></i>
+            </div>
+        </div>
+    </header>
+
+</body>
+</html>
+
